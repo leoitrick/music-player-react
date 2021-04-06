cbaa2dcfc00848b69f0fa6a26fdc0e0221be0006
Leandro Moreira <leoitrick@Leandros-MacBook-Pro.local>
2021-04-06 07:52:38 -0400

    add a file with songs and made it play songs

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./styles/app.scss";
 
 import Player from "./components/Player";
 import Song from "./components/Song";
 
+import data from "./util";
+
 function App() {
+  //state - getting the songs from util.js file
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
+
   return (
     <div className="App">
-      <Song />
-      <Player />
+      <Song currentSong={currentSong} />
+      <Player
+        isPlaying={isPlaying}
+        setIsPlaying={setIsPlaying}
+        currentSong={currentSong}
+      />
     </div>
   );
 }
