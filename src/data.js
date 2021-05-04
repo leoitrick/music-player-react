db14c6ef8840e137c1da686a7c0d8eb7a1daf184
Leandro Moreira <leoitrick@Leandros-MacBook-Pro.local>
2021-05-03 21:11:09 -0400

    added some songs

--- src/data.js
+++ src/data.js
@@ -22,6 +22,86 @@ function chillHop() {
       id: uuidv4(),
       active: false,
     },
+    {
+      name: "Roses n Flames",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/02/d12927eedcc2f5afba2ab049a4a1ea46c2266ae3-1024x1024.jpg",
+      artist: "C Y G N",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=14984",
+      color: ["#2E394D", "#E996F8"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Skates",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/03/74d62bc9370a68e440c1b98eaf650344f0a7faea-1024x1024.jpg",
+      artist: "SwuM",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=15225",
+      color: ["#443A2B", "#273633"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Soul Samba",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/03/630fbe3721ce9fa27e56b6b01c460f709fbce3b7-1024x1024.jpg",
+      artist: "Screen Jazzmaster, Zmeyev",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=16061",
+      color: ["#40BAA2", "#F6C062"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Hereafter",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/11/f78c39b4bb6313ddd0354bef896c591bfb490ff8-1024x1024.jpg",
+      artist: "Makzo",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=11767",
+      color: ["#5668A0", "#FFDE77"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Through Trees",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/11/8a0c857ddad531279d0757f5362380a6837b1b69-1024x1024.jpg",
+      artist: "Sleepy Fish, The Field Tapes",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=11250",
+      color: ["#969F7A", "#253824"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Going Back",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/10/737bb830d34592344eb4a2a1d2c006cdbfc811d9-1024x1024.jpg",
+      artist: "Swørn",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=10307",
+      color: ["#2A375E", "#355562"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Mirage",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/09/09fb436604242df99f84b9f359acb046e40d2e9e-1024x1024.jpg",
+      artist: "Nymano, j'san",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=10132",
+      color: ["#8A6786", "#5B5285"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Bliss",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/09/5bff1a6f1bd0e2168d29b4c841b811598135e457-1024x1024.jpg",
+      artist: "Misha, Jussi Halme",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=6817",
+      color: ["#F6949E", "#122945"],
+      id: uuidv4(),
+      active: false,
+    },
   ];
 }
 
